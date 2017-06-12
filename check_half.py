e1fb748fc2a97dd9b4cc6bb2916f1725daedeeae
Jeff Zemla <jeffzemla@gmail.com>
2017-06-12 17:39:09 -0500

    mixture model
    Former-commit-id: c445d926b348a4423d570523ce6950bf4e968774 [formerly 53395e2d92ebf51e3487ef6f3497f19318732ecf]
    Former-commit-id: ff467be3eca1924f19387d6e32b63d833baef134
    Former-commit-id: 51e8e6fc34cbfd56c6d74e030bc3a1196ebac0a0
    Former-commit-id: ca1cdf50a56fe04508e531e72701828243b928d1

--- check_half.py
+++ check_half.py
@@ -12,7 +12,7 @@ td=rw.Data({
         'trim': 35})
 
 
-fh = open('zip.pickle','r')
+fh = open('densitymodel.pickle','r')
 alldata = pickle.load(fh)
 Xs = alldata['datab'][0:10]
 graphs = alldata['uinvite_graphs']
